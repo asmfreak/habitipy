69383f83e7f6cb6ab184536ffd86c3f7a23c2f21
ASM <cpp.create@gmail.com>
2017-08-22 11:28:37 +0200

    Fix hanging due to new fucnctions test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest.mock import patch, MagicMock
+from contextlib import ExitStack
 import os
+import uuid
 
 from habitipy.cli import load_conf
 from habitipy.util import secure_filestore, SecurityError, assert_secure_file, is_secure_file
@@ -29,9 +32,23 @@ class TestFileUtils(unittest.TestCase):
 
     def test_load_conf(self):
         os.remove(self.filename)
-        with self.assertWarns(UserWarning):
+        with patch('plumbum.cli.terminal.ask', MagicMock(return_value=False)) as m:
             conf = load_conf(self.filename)
+            self.assertTrue(m.called)
         self.assertEqual(conf['url'], 'https://habitica.com')
+        self.assertTrue(is_secure_file(self.filename))
+        os.remove(self.filename)
+        with ExitStack() as s:
+            ask = s.enter_context(
+                patch('plumbum.cli.terminal.ask', MagicMock(return_value=True)))
+            prompt = s.enter_context(
+                patch('plumbum.cli.terminal.prompt', MagicMock(return_value='TEST_DATA')))
+            conf = load_conf(self.filename)
+            self.assertEqual(ask.call_count, 1)
+            self.assertEqual(prompt.call_count, 2)
+        self.assertEqual(conf['url'], 'https://habitica.com')
+        self.assertEqual(conf['login'], 'TEST_DATA')
+        self.assertEqual(conf['password'], 'TEST_DATA')
         self.assertTrue(is_secure_file(self.filename))
         os.chmod(self.filename, 0o666)
         with self.assertRaises(SecurityError):
